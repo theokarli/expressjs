3e68cbe92f79a0657344811b2305c2d5c0f96ab2
Theo <100994644+theokarli@users.noreply.github.com>
2022-10-14 16:35:20 +0800

    Update lagi di branchh fitur tumballl

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ Hello: "World Theo Super SEKALII" });
+  res.json({ Hello: "World Theo Super SEKALII 2" });
 });
 
 app.listen(port, () => {
