38428b2b6a2277c144ca628be50351816d4386ef
Theo <100994644+theokarli@users.noreply.github.com>
2022-10-14 17:31:39 +0800

    menambahkan data theodorus karli dari kampus mana

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ Hello: "Theodorus Karli" });
+  res.json({ Hello: "Theodorus Karli anak dari kampus STMIK KHARISMA MAKASSAR " });
 });
 
 app.listen(port, () => {
